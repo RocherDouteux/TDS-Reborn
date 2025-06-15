4fd5aa6fecd78ff564a8dedaf9ee6bdb945c5b6d
StealYourCode <ksporent@gmail.com>
2025-06-15 19:05:42 +0200

    Ajout de commentaire pour la partie application

--- src/Application/Application.java
+++ src/Application/Application.java
@@ -23,94 +23,111 @@ public class Application {
     
     
     public static Queue<CImage> question1A(CImage image){
-        Queue<CImage> sequence = new LinkedList();
+        Queue<CImage> sequence = new LinkedList<>();
         sequence.add(image);
-        try{
+
+        try {
             if(image instanceof CImageNG cImageNG){
                 int[][] data = cImageNG.getMatrice();
-                
+
+                // Step 1 – Application du filtre médian 5×5
                 int[][] output = MorphoComplexe.filtreMedian(data, 5);
                 sequence.add(new CImageNG(output));
-                
+
+                // Step 2 – Application du filtre médian 9×9
                 output = MorphoComplexe.filtreMedian(data, 9);
                 sequence.add(new CImageNG(output));
-
             }
-            
+
         } catch (CImageNGException ex) {
             logger.log(Level.SEVERE, null, ex);
         }
-        
+
         return sequence;
     }
+
     
-    public static Queue<CImage> question1B(CImage image){
-        Queue<CImage> sequence = new LinkedList();
+    public static Queue<CImage> question1B(CImage image) {
+        Queue<CImage> sequence = new LinkedList<>();
         sequence.add(image);
-        
+
         try {
-            if(image instanceof CImageNG imageNG){
+            if (image instanceof CImageNG imageNG) {
+
+                // Step 1 – Application du filtre médian 3×3 une première fois
                 int[][] median3 = MorphoComplexe.filtreMedian(imageNG.getMatrice(), 3);
                 sequence.add(new CImageNG(median3));
-                
+
+                // Step 2 – Application du filtre médian 3×3 une seconde fois
                 median3 = MorphoComplexe.filtreMedian(median3, 3);
                 sequence.add(new CImageNG(median3));
             }
-        }catch(CImageNGException ex){
+
+        } catch (CImageNGException ex) {
             logger.log(Level.SEVERE, null, ex);
         }
-        
+
         return sequence;
     }
+
     
-    public static Queue<CImage> question2A(CImage image){
-        Queue<CImage> sequence = new LinkedList();
+    public static Queue<CImage> question2A(CImage image) {
+        Queue<CImage> sequence = new LinkedList<>();
         sequence.add(image);
-        
+
         try {
-            if(image instanceof CImageRGB imageRGB){
+            if (image instanceof CImageRGB imageRGB) {
+
+                // Step 1 – Extraction des canaux R, G et B
                 int[][] redChannel = Utils.extraireCanal(imageRGB, "red");
                 int[][] greenChannel = Utils.extraireCanal(imageRGB, "green");
                 int[][] blueChannel = Utils.extraireCanal(imageRGB, "blue");
-                
+
+                // Step 2 – Création des courbes tonales pour égalisation (R, G, B)
                 int[] redCurve = Histogramme.creeCourbeTonaleEgalisation(redChannel);
                 int[] greenCurve = Histogramme.creeCourbeTonaleEgalisation(greenChannel);
                 int[] blueCurve = Histogramme.creeCourbeTonaleEgalisation(blueChannel);
-                
+
+                // Step 3 – Application des courbes sur chaque canal (rehaussement)
                 int[][] egalisationRed = Histogramme.rehaussement(redChannel, redCurve);
                 egalisationRed = Utils.normaliserImage(egalisationRed, 0, 255);
-                
+
                 int[][] egalisationGreen = Histogramme.rehaussement(greenChannel, greenCurve);
                 egalisationGreen = Utils.normaliserImage(egalisationGreen, 0, 255);
-                
+
                 int[][] egalisationBlue = Histogramme.rehaussement(blueChannel, blueCurve);
                 egalisationBlue = Utils.normaliserImage(egalisationBlue, 0, 255);
-                
+
+                // Step 4 – Ajout des canaux égalisés (niveaux de gris) à la séquence
                 sequence.add(new CImageNG(egalisationRed));
                 sequence.add(new CImageNG(egalisationGreen));
                 sequence.add(new CImageNG(egalisationBlue));
-                
+
+                // Step 5 – Conversion des canaux égalisés en images RGB colorées
                 CImageRGB backToRed = Utils.convertionNGToRGB(egalisationRed, "red");
                 CImageRGB backToGreen = Utils.convertionNGToRGB(egalisationGreen, "green");
                 CImageRGB backToBlue = Utils.convertionNGToRGB(egalisationBlue, "blue");
-                
+
                 sequence.add(backToRed);
                 sequence.add(backToGreen);
                 sequence.add(backToBlue);
-                
+
+                // Step 6 – Fusion du rouge et du vert pour obtenir du jaune
                 CImageRGB fusionRedAndGreen = Utils.additionRGB(backToRed, backToGreen);
                 sequence.add(fusionRedAndGreen);
-                
+
+                // Step 7 – Fusion du jaune et du bleu pour reconstituer une image RGB égalisée
                 CImageRGB fusionYellowAndBlue = Utils.additionRGB(fusionRedAndGreen, backToBlue);
                 sequence.add(fusionYellowAndBlue);
-
             }
-        }catch(CImageRGBException | CImageNGException ex){
+
+        } catch (CImageRGBException | CImageNGException ex) {
             logger.log(Level.SEVERE, null, ex);
         }
-        
+
         return sequence;
     }
+
     
     public static Queue<CImage> question2B(CImage image, CImageNG imageNG){
         Queue<CImage> sequence = new LinkedList();
@@ -389,102 +406,121 @@ public static Queue<CImage> question6A(CImage image, CImage RGBImage, CImage sec
     return sequence;
 }
 
-public static Queue<CImage> question6B(CImage image, CImage RGBImage, CImage second) {
-    Queue<CImage> sequence = new LinkedList<>();
-    sequence.add(image);
-    sequence.add(RGBImage);
-    sequence.add(second);
+    public static Queue<CImage> question6B(CImage image, CImage RGBImage, CImage second) {
+        Queue<CImage> sequence = new LinkedList<>();
+        sequence.add(image);
+        sequence.add(RGBImage);
+        sequence.add(second);
 
-    try {
-        if (image instanceof CImageNG imageNG && RGBImage instanceof CImageRGB shipRGB && second instanceof CImageRGB planetRGB) {
-                        // Same same as Question6A sadly
-            int[][] vaissGray = imageNG.getMatrice();
-            int[][] seuil60 = Seuillage.seuillageSimple(vaissGray, 60);
-            int[][] eroded = MorphoElementaire.erosion(seuil60, 21);
-            int[][] recon = MorphoComplexe.reconstructionGeodesique(eroded, seuil60);
-            int[][] sub = Utils.soustraction(seuil60, recon);
-            int[][] opened = MorphoElementaire.ouverture(sub, 11);
-            int[][] finalMask = MorphoComplexe.reconstructionGeodesique(opened, vaissGray);
+        try {
+            if (image instanceof CImageNG imageNG && RGBImage instanceof CImageRGB shipRGB && second instanceof CImageRGB planetRGB) {
 
-            // Compute red contour around the ship
-            int[][] gradH = ContoursLineaire.gradientPrewitt(finalMask, 1);
-            int[][] gradV = ContoursLineaire.gradientPrewitt(finalMask, 2);
-            int[][] edge = Utils.addition(gradH, gradV);
-            edge = Utils.normaliserImage(edge, 0, 255);
-            int[][] contour = Seuillage.seuillageSimple(edge, 50);
-            CImageNG contourNG = new CImageNG(contour);
-            sequence.add(contourNG);
-            
-            // Get spaceship contour
-            int [][]uniShip = MorphoElementaire.fermeture(contour, 7);
-            int[][] prewittH = ContoursLineaire.gradientPrewitt(uniShip, 1);
-            int[][] prewittV = ContoursLineaire.gradientPrewitt(uniShip, 2);
-            
-            
-            int[][] additionPrewitt = Utils.addition(prewittH, prewittV);
-            sequence.add(new CImageNG(additionPrewitt));
-            
-            int[][] filledContourNG = MorphoElementaire.fermeture(contour, 9);
-            // sequence.add(new CImageNG(filledContourNG));
-            
-            int[] negativeCurve = Histogramme.creeCourbeTonaleNegatif();
-            int[][] negative = Histogramme.rehaussement(filledContourNG, negativeCurve);
-            
-            CImageRGB smallShip = Utils.andRGBWithMask(shipRGB, new CImageNG(filledContourNG));
-            CImageRGB holed = Utils.andRGBWithMask(planetRGB, new CImageNG(negative));
-            
-            // Convert contour to red
-            CImageRGB redContour = Utils.convertionNGToRGB(additionPrewitt, "red");
-            //sequence.add(redContour);
+                int[][] vaissGray = imageNG.getMatrice();
+                int[][] seuil60 = Seuillage.seuillageSimple(vaissGray, 60);
+                int[][] eroded = MorphoElementaire.erosion(seuil60, 21);
+                int[][] recon = MorphoComplexe.reconstructionGeodesique(eroded, seuil60);
+                int[][] sub = Utils.soustraction(seuil60, recon);
+                int[][] opened = MorphoElementaire.ouverture(sub, 11);
+                int[][] finalMask = MorphoComplexe.reconstructionGeodesique(opened, vaissGray);
 
-            CImageRGB fusion = Utils.additionRGB(holed, smallShip);
-            sequence.add(fusion);
+                // Step 1 – Calcul des contours du masque (Prewitt H + V)
+                int[][] gradH = ContoursLineaire.gradientPrewitt(finalMask, 1);
+                int[][] gradV = ContoursLineaire.gradientPrewitt(finalMask, 2);
+                int[][] edge = Utils.addition(gradH, gradV);
+                edge = Utils.normaliserImage(edge, 0, 255);
 
-            // Add red contour to fusion
-            CImageRGB finalResult = Utils.additionRGB(fusion, redContour);
-            sequence.add(finalResult);
+                // Step 2 – Seuillage des contours à 50
+                int[][] contour = Seuillage.seuillageSimple(edge, 50);
+                CImageNG contourNG = new CImageNG(contour);
+                sequence.add(contourNG);
+
+                // Step 3 – Fermeture pour uniformiser les contours du vaisseau
+                int[][] uniShip = MorphoElementaire.fermeture(contour, 7);
+
+                // Step 4 – Calcul du gradient Prewitt sur le contour fermé
+                int[][] prewittH = ContoursLineaire.gradientPrewitt(uniShip, 1);
+                int[][] prewittV = ContoursLineaire.gradientPrewitt(uniShip, 2);
+                int[][] additionPrewitt = Utils.addition(prewittH, prewittV);
+                sequence.add(new CImageNG(additionPrewitt));
+
+                // Step 5 – Fermeture pour remplir les trous dans le contour
+                int[][] filledContourNG = MorphoElementaire.fermeture(contour, 9);
+                // sequence.add(new CImageNG(filledContourNG));
+
+                // Step 6 – Création d’une courbe de négatif et application sur le contour rempli
+                int[] negativeCurve = Histogramme.creeCourbeTonaleNegatif();
+                int[][] negative = Histogramme.rehaussement(filledContourNG, negativeCurve);
+
+                // Step 7 – Extraction du vaisseau depuis l’image RGB via masque
+                CImageRGB smallShip = Utils.andRGBWithMask(shipRGB, new CImageNG(filledContourNG));
+
+                // Step 8 – Application du négatif pour créer un "trou" dans la planète
+                CImageRGB holed = Utils.andRGBWithMask(planetRGB, new CImageNG(negative));
+
+                // Step 9 – Conversion des contours en rouge
+                CImageRGB redContour = Utils.convertionNGToRGB(additionPrewitt, "red");
+                // sequence.add(redContour);
+
+                // Step 10 – Fusion du vaisseau et de la planète trouée
+                CImageRGB fusion = Utils.additionRGB(holed, smallShip);
+                sequence.add(fusion);
+
+                // Step 11 – Ajout des contours rouges à l’image finale
+                CImageRGB finalResult = Utils.additionRGB(fusion, redContour);
+                sequence.add(finalResult);
+            }
+
+        } catch (CImageNGException | CImageRGBException ex) {
+            logger.log(Level.SEVERE, null, ex);
         }
 
-    } catch (CImageNGException | CImageRGBException ex) {
+        return sequence;
+    }
+
+
+public static Queue<CImage> question7(CImage image, CImage output){
+    Queue<CImage> sequence = new LinkedList<>();
+    sequence.add(image);
+
+    try {
+        if(image instanceof CImageNG imageNG && output instanceof CImageRGB imageRGB){
+
+            // Step 1 – Calcul Prewitt horizontal (Prewitt H)
+            int[][] prewittH = ContoursLineaire.gradientPrewitt(imageNG.getMatrice(), 1);
+            prewittH = Utils.normaliserImage(prewittH, 0, 255);
+            sequence.add(new CImageNG(prewittH));
+
+            // Step 2 – Calcul Prewitt vertical (Prewitt V)
+            int[][] prewittV = ContoursLineaire.gradientPrewitt(imageNG.getMatrice(), 2);
+            prewittV = Utils.normaliserImage(prewittV, 0, 255);
+            sequence.add(new CImageNG(prewittV));
+
+            // Step 3 – Addition des deux Prewitt (H + V)
+            int[][] addition = Utils.addition(prewittH, prewittV);
+            sequence.add(new CImageNG(addition));
+
+            // Step 4 – Seuillage double (détection des contours)
+            int[][] seuillage = Seuillage.seuillageDouble(addition, 80, 90);
+            sequence.add(new CImageNG(seuillage));
+
+            // Step 5 – Dilatation des contours détectés
+            int[][] dilatation = MorphoElementaire.dilatation(seuillage, 3);
+            sequence.add(new CImageNG(dilatation));
+
+            // Step 6 – Conversion en image RGB avec contours en vert
+            CImageRGB contoursVerts = Utils.convertionNGToRGB(dilatation, "green");
+            sequence.add(contoursVerts);
+
+            // Step 7 – Fusion de l’image RGB d’origine avec les contours verts
+            CImageRGB fusion = Utils.additionRGB(imageRGB, contoursVerts);
+            sequence.add(fusion);
+
+        }
+    } catch(CImageNGException | CImageRGBException ex){
         logger.log(Level.SEVERE, null, ex);
     }
 
     return sequence;
 }
 
-    
-    public static Queue<CImage> question7(CImage image, CImage output){
-        Queue<CImage> sequence = new LinkedList();
-        sequence.add(image);
-        
-        try {
-            if(image instanceof CImageNG imageNG && output instanceof CImageRGB imageRGB){
-                int[][] prewittH = ContoursLineaire.gradientPrewitt(imageNG.getMatrice(), 1);
-                prewittH = Utils.normaliserImage(prewittH, 0, 255);
-                int[][] prewittV = ContoursLineaire.gradientPrewitt(imageNG.getMatrice(), 2);
-                prewittV = Utils.normaliserImage(prewittV, 0, 255);
-                int[][] addition = Utils.addition(prewittH, prewittV);
-                
-                sequence.add(new CImageNG(prewittH));
-                sequence.add(new CImageNG(prewittV));
-                sequence.add(new CImageNG(addition));
-                
-                int[][] seuillage = Seuillage.seuillageDouble(addition, 80, 90);
-                sequence.add(new CImageNG(seuillage));
-                
-                int[][] dilatation = MorphoElementaire.dilatation(seuillage, 3);
-                sequence.add(new CImageNG(dilatation));
-                
-                CImageRGB contoursVerts = Utils.convertionNGToRGB(dilatation, "green");
-                sequence.add(contoursVerts);
-                
-                CImageRGB fusion = Utils.additionRGB(imageRGB, contoursVerts);
-                sequence.add(fusion);
-            }
-        }catch(CImageNGException | CImageRGBException ex){
-            logger.log(Level.SEVERE, null, ex);
-        }
-        
-        return sequence;
-    }
 }
